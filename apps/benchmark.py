d01f4c0f5378a5b6e3ca0a425797fd6e96af637a
dsuarez01 <dsuarez@mit.edu>
2025-11-14 01:50:05 -0500

    Test suite mostly complete, only requires (1) finishing dequant; (2) calling the actual kernels we wrote

--- apps/benchmark.py
+++ apps/benchmark.py
@@ -1,1 +1,2 @@
-# TODO: complete
+# TODO: complete 
+# NOTE: compare torch default CUDA opns, Triton, and custom CUDA opns
--- tests/test_kernels.py
+++ tests/test_kernels.py
@@ -1,10 +1,21 @@
 # TODO: add me
 import torch
+import torch.nn.functional as F
 import pytest
 
 from minfer.kernels import KernelBackend
 from minfer.kernels.triton.kernels import _dequant as triton_dequant
 from minfer.kernels.cuda.kernels import _dequant as cuda_dequant # type: ignore
+
+def pytest_addoption(parser):
+    parser.addoption("--seed", default=42, type=int)
+
+@pytest.fixture(autouse=True)
+def seed(request):
+    seed_val = request.config.getoption("--seed")
+    torch.manual_seed(seed_val)
+    torch.cuda.manual_seed(seed_val)
+    print(f"Using seed: {seed_val}") # always print out the seed, for user reference
 
 pytestmark = pytest.mark.skipif(not torch.cuda.is_available(), reason="GPU required")
 
@@ -80,72 +91,176 @@ def test_dequant(backend, dequant):
 
     return
 
-## NOTE: for the rest of the tests FP32 / FP16 dtype tensors are used (as appropriate)
+## NOTE: for the rest of the tests FP16 dtype tensors are used (as appropriate)
 ## since dequant already tests the relevant usage patterns in the other kernels
 ## dp_size is used throughout but not actually factored in since it doesn't affect kernel usage
 
+# A: [B, L, hidden_dim]
+# B: [B, n_heads, L, head_dim]
 @pytest.mark.parametrize("backend", ["triton", "cuda"])
 def test_rmsnorm(backend):
     kerns = KernelBackend(backend)
-    B, L, hidden_dim = 8, 4096, 6144 # adjust as needed
-    input = torch.randn((B,L,hidden_dim), dtype=torch.float16)
-    weight = torch.randn((hidden_dim), dtype=torch.float16)
+    B, L, hidden_dim, n_heads, head_dim, eps = 8, 4096, 6144, 48, 128, 1e-6 # adjust as needed
+    
     # test for rmsnorm applied across entire vector (act. shape [B // dp_size, L, hidden_dim], weight shape [hidden_dim,])
+    input_A = torch.randn((B*L,hidden_dim), dtype=torch.float16).cuda()
+    actual_A = torch.zeros_like(input_A)
+    weight_A = torch.randn((hidden_dim), dtype=torch.float16).cuda()
+    expected_A = F.rms_norm(input=input_A, weight=weight_A, normalized_shape=(hidden_dim,),  eps=eps)
+    # TODO: add rmsnorm call on actual_A here
+    assert torch.allclose(expected_A, actual_A), "rmsnorm: over entire vec"
 
     # test for rmsnorm applied across heads (act. shape [B // dp_size, n_heads, L, head_dim], weight shape [head_dim,])
+    input_B = torch.randn((B*n_heads,L,head_dim), dtype=torch.float16).cuda()
+    actual_B = torch.zeros_like(input_B)
+    weight_B = torch.randn((head_dim,), dtype=torch.float16).cuda()
+    expected_B = F.rms_norm(input=input_B, weight=weight_B, normalized_shape=(head_dim,),  eps=eps)
+    # TODO: add rmsnorm call on actual_B here
+    assert torch.allclose(expected_B, actual_B), "rmsnorm: per head"
 
-    # in both cases: output act. shape identical to input
+    # output act. identical shape to input in both cases
 
 @pytest.mark.parametrize("backend", ["triton", "cuda"])
 def test_rope(backend):
     kerns = KernelBackend(backend)
+    B, L, n_heads, head_dim, base_freq = 8, 4096, 48, 128, 1e6 # adjust as needed
 
     # test for rope applied across heads (act. shape [B // dp_size, n_heads, L, head_dim])
-    # output act. shape identical to input
+    input_A = torch.randn((B*n_heads,L,head_dim), dtype=torch.float16).cuda()
+    actual_A = torch.zeros_like(input_A).cuda()
+
+    # computing expected case (interleaved)
+    freqs_A = torch.arange(head_dim // 2, dtype=torch.float16).cuda()
+    freqs_A = base_freq ** (-2.0 * freqs_A / head_dim)
+    freqs_A = torch.arange(L, dtype=torch.float16)[:, None].cuda() * freqs_A[None,:]
+    
+    input_A = torch.view_as_complex(input_A.reshape(*input_A.shape[:-1], -1, 2))
+    freqs_A = torch.view_as_complex(freqs_A)
+    expected_A = torch.view_as_real(input_A * freqs_A).flatten(-2)
+
+    # TODO: add rope call on actual_A here
+    assert torch.allclose(expected_A, actual_A), "rope: interleaved"
+
+    # TODO: support neox/two-halves later too
+
+    # output act. identical shape to input
 
 @pytest.mark.parametrize("backend", ["triton", "cuda"])
 def test_matmul(backend):
     kerns = KernelBackend(backend)
+    B, L, in_dim, out_dim = 8, 4096, 6144, 16384
 
-    # act. shape [B // dp_size, L, dim_in], weight shape [dim_out, dim_in]
-    # output act. shape [B // dp_size, L, dim_out]
+    # act. shape [B // dp_size, L, in_dim], weight shape [out_dim, in_dim]    
+    input_A = torch.randn((B*L, in_dim), dtype=torch.float16).cuda()
+    actual_A = torch.zeros_like(input_A)
+    weight_A = torch.randn((out_dim, in_dim), dtype=torch.float16).cuda()
+
+    expected_A = input_A @ weight_A.T
+    # TODO: add matmul call on actual_A here
+    assert torch.allclose(expected_A, actual_A), "matmul"
+
+    # output act. shape [B // dp_size, L, out_dim]
 
 @pytest.mark.parametrize("backend", ["triton", "cuda"])
 def test_embed(backend):
     kerns = KernelBackend(backend)
+    B, L, vocab_size, hidden_dim = 8, 4096, 128_000, 6144
 
     # act. shape [B // dp_size, L], weight shape [vocab_size, hidden_dim]
+    input_A = torch.randint(0, vocab_size, (B,L)).cuda()
+    actual_A = torch.zeros((B, L, hidden_dim), dtype=torch.float16).cuda()
+    weight_A = torch.randn((vocab_size, hidden_dim), dtype=torch.float16).cuda()
+
+    expected_A = weight_A[input_A]
+    # TODO: add embed call on actual_A here
+    assert torch.allclose(expected_A, actual_A), "embed"
+    
     # output act. shape [B // dp_size, L, hidden_dim]
 
 @pytest.mark.parametrize("backend", ["triton", "cuda"])
 def test_qkv(backend):
     kerns = KernelBackend(backend)
+    B, L, hidden_dim, n_heads, n_kv_heads, head_dim = 8, 4096, 6144, 48, 8, 128
 
     # act. shape [B // dp_size, L, hidden_dim], weight shape [q_dim or kv_dim, hidden_dim]
+    input_A = torch.randn((B,L,hidden_dim), dtype=torch.float16).cuda()
+    actual_A_Q = torch.zeros((B,n_heads,L,head_dim), dtype=torch.float16).cuda()
+    actual_A_K = torch.zeros((B,n_kv_heads,L,head_dim), dtype=torch.float16).cuda()
+    actual_A_V = torch.zeros((B,n_kv_heads,L,head_dim), dtype=torch.float16).cuda()
+    
+    weight_A_Q = torch.randn((n_heads*head_dim,hidden_dim), dtype=torch.float16).cuda()
+    weight_A_K = torch.randn((n_kv_heads*head_dim,hidden_dim), dtype=torch.float16).cuda()
+    weight_A_V = torch.randn((n_kv_heads*head_dim,hidden_dim), dtype=torch.float16).cuda()
+
+    expected_A_Q = (input_A @ weight_A_Q.T).reshape(B, L, n_heads, head_dim).transpose(1,2)
+    expected_A_K = (input_A @ weight_A_K.T).reshape(B, L, n_kv_heads, head_dim).transpose(1,2)
+    expected_A_V = (input_A @ weight_A_V.T).reshape(B, L, n_kv_heads, head_dim).transpose(1,2)
+
+    # TODO: add qkv call on actual_Q,K,V here
+
+    assert torch.allclose(expected_A_Q, actual_A_Q), "qkv: Q"
+    assert torch.allclose(expected_A_K, actual_A_K), "qkv: K"
+    assert torch.allclose(expected_A_V, actual_A_V), "qkv: V"
+
     # output: Q shape [B // dp_size, n_heads, L, head_dim], K and V shape [B // dp_size, n_kv_heads, L, head_dim]
 
 
 @pytest.mark.parametrize("backend", ["triton", "cuda"])
 def test_flash_attn(backend):
     kerns = KernelBackend(backend)
+    B, L, hidden_dim, n_heads, n_kv_heads, head_dim = 8, 4096, 6144, 48, 8, 128
 
     # Q shape [B // dp_size, n_heads, L, head_dim], K and V shape [B // dp_size, n_kv_heads, L, head_dim]
+    input_A_Q = torch.zeros((B,n_heads,L,head_dim), dtype=torch.float16).cuda()
+    input_A_K = torch.zeros((B,n_kv_heads,L,head_dim), dtype=torch.float16).cuda()
+    input_A_V = torch.zeros_like(input_A_K)
+
+    actual_A = torch.zeros((B,L,hidden_dim), dtype=torch.float16).cuda()
+    expected_A = F.scaled_dot_product_attention(input_A_Q, input_A_K, input_A_V)
+
+    # TODO: add flash_attn call on actual_A here
+
+    assert torch.allclose(expected_A, actual_A), "flash_attn"
+    
     # output act. shape [B // dp_size, L, hidden_dim]
-    # can use F.scaled_dot_product_attention from pytorch for this?
 
 @pytest.mark.parametrize("backend", ["triton", "cuda"])
 def test_moe_scoring(backend):
     kerns = KernelBackend(backend)
+    B, L, n_experts, hidden_dim = 8, 4096, 8, 6144
 
     # performs matmul and fused (online) softmax
     # input act. shape [B // dp_size, L, hidden_dim]
-    # output act. shape [B // dp_size, L, n_experts]
+    input_A = torch.zeros((B,L,hidden_dim), dtype=torch.float16).cuda()
+    actual_A = torch.zeros((B,L,n_experts), dtype=torch.float16).cuda()
+    weight_A = torch.randn((n_experts, hidden_dim), dtype=torch.float16).cuda()
 
+    expected_A = F.softmax(input_A @ weight_A.T, dim=-1)
+
+    # TODO: add moe_scoring call on actual_A here
+
+    assert torch.allclose(expected_A, actual_A), "moe_scoring"
+
+    # output act. shape [B // dp_size, L, n_experts]
 
 @pytest.mark.parametrize("backend", ["triton", "cuda"])
 def test_ffn(backend):
     kerns = KernelBackend(backend)
+    B, L, hidden_dim, mlp_dim = 8, 4096, 6144, 16384
 
     # performs SwiGLU then downproj.
     # input act. shape [B // dp_size, L, hidden_dim]
+    input_A = torch.zeros((B,L,hidden_dim), dtype=torch.float16).cuda()
+    actual_A = torch.zeros_like(input_A)
+
+    weight_A_gate = torch.randn((mlp_dim, hidden_dim), dtype=torch.float16).cuda()
+    weight_A_up = torch.randn((mlp_dim, hidden_dim), dtype=torch.float16).cuda()
+    weight_A_down = torch.randn((hidden_dim, mlp_dim), dtype=torch.float16).cuda()
+
+    expected_A = (F.silu((input_A @ weight_A_gate.T)) * (input_A @ weight_A_up.T)) @ weight_A_down.T
+
+    # TODO: add ffn call on actual_A here
+
+    assert torch.allclose(expected_A, actual_A), "ffn"
+
     # output act. shape [B // dp_size, L, hidden_dim]
