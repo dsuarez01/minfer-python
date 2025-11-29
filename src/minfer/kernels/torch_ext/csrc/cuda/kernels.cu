f99b12f456e0db7430d5839d5a088df98ef9e41e
DS <dsuarez@mit.edu>
2025-11-29 16:00:30 -0500

    Added il and neox rope

--- src/minfer/kernels/torch_ext/csrc/cuda/kernels.cu
+++ src/minfer/kernels/torch_ext/csrc/cuda/kernels.cu
@@ -59,10 +59,13 @@ __global__ void rmsnorm_cuda_impl(
 void rmsnorm_cuda(const at::Tensor& in, at::Tensor& out, const at::Tensor& w, float eps) {
     // checks
     TORCH_CHECK(in.shape() == out.shape());
-    TORCH_CHECK(in.size(-1) == w.size(0) || in.size(-1) % w.size(0) == 0);
+    TORCH_CHECK(in.dims() == 3 || in.dims() == 4);
+    TORCH_CHECK(in.size(-1) == w.size(0) || in.size(-1) % w.size(0) == 0); // per-head or per-entire vector
+    
     TORCH_CHECK(in.dtype() == at::kHalf);
     TORCH_CHECK(out.dtype() == at::kHalf);
     TORCH_CHECK(w.dtype() == at::kHalf);
+    
     TORCH_INTERNAL_ASSERT(in.device().type() == at::DeviceType::CUDA);
     TORCH_INTERNAL_ASSERT(out.device().type() == at::DeviceType::CUDA);
     
@@ -70,29 +73,120 @@ void rmsnorm_cuda(const at::Tensor& in, at::Tensor& out, const at::Tensor& w, fl
     TORCH_CHECK(out.is_contiguous());
     TORCH_CHECK(w.is_contiguous());
 
-    int dim = w.size(0);
     const half* in_ptr = in.data_ptr<at::Half>();
     half* out_ptr = out.data_ptr<at::Half>();
     const half* w_ptr = w.data_ptr<at::Half>();
 
-    const int n_norms = in.size(0) * in.size(1) * (in.size(-1) / dim);
+    // handles both [B,L,D] and [B,L,n_heads,head_dim]
+    int dim = w.size(0);
+    int n_blocks = in.numel() / dim;
 
     cudaStream_t stream = at::cuda::getCurrentCUDAStream();
-    rmsnorm_cuda_impl<<<n_norms, 1024, 0, stream>>>(in_ptr, out_ptr, w_ptr, dim, eps);
+    rmsnorm_cuda_impl<<<n_blocks, 1024, 0, stream>>>(in_ptr, out_ptr, w_ptr, dim, eps);
+}
+
+__global__ void il_rope_cuda_impl (
+    half* __restrict__ x,
+    int n_heads,
+    int rotary_dim,
+    int head_dim,
+    int start_pos,
+    float freq_base
+) {
+    int head_idx = blockIdx.z % n_heads;
+    int pair_idx = (blockIdx.z / n_heads) * 32 + threadIdx.x;
+    int pos = start_pos + blockIdx.y;
+    
+    if (pair_idx >= rotary_dim / 2) return;
+
+    half* x_head = x + (blockIdx.x * gridDim.y * n_heads + blockIdx.y * n_heads + head_idx)*head_dim;
+
+    float freq = 1.0f / pow(freq_base, 2.0f * pair_idx / rotary_dim);
+    float angle = pos * freq;
+
+    int idx = 2*pair_idx;
+
+    float x_0 = __half2float(x_head[idx]);
+    float x_1 = __half2float(x_head[idx+1]);
+
+    x_head[idx] = __float2half(cos(angle)*x_0 - sin(angle)*x_1);
+    x_head[idx+1] = __float2half(sin(angle)*x_0 + cos(angle)*x_1);
 }
 
 void il_rope_cuda(
-    const at::Tensor& x,
-    int d_rotary,
-    int d_head,
-    float freq_base,
-
+    at::Tensor& x, // [B,L,n_heads,head_dim]
+    int rotary_dim,
+    int head_dim,
+    int start_pos,
+    float freq_base
 ) {
-    TORCH_CHECK(false, "il_rope not implemented");
+    TORCH_CHECK(rotary_dim <= head_dim);
+    TORCH_CHECK(x.dtype() == at::kHalf);
+    TORCH_CHECK(x.dim() == 4);
+    TORCH_CHECK(x.is_contiguous());
+
+    half* x_ptr = x.data_ptr<at::Half>();
+
+    int B = x.size(0);
+    int L = x.size(1);
+    int n_heads = x.size(2);
+    int head_dim = x.size(3);
+
+    dim3 grid(B, L, n_heads*(rotary_dim/2+31)/32);
+
+    cudaStream_t stream = at::cuda::getCurrentCUDAStream();
+    il_rope_cuda_impl<<<grid, 32, 0, stream>>>(x_ptr, n_heads, rotary_dim, head_dim, start_pos, freq_base);
 }
 
-void neox_rope_cuda() {
-    TORCH_CHECK(false, "neox_rope not implemented");
+__global__ void neox_rope_cuda_impl(
+    half* __restrict__ x,
+    int n_heads,
+    int rotary_dim,
+    int head_dim,
+    int start_pos,
+    float freq_base
+) {
+    int head_idx = blockIdx.z % n_heads;
+    int pair_idx = (blockIdx.z / n_heads) * 32 + threadIdx.x;
+    int pos = start_pos + blockIdx.y;
+    
+    if (pair_idx >= rotary_dim / 2) return;
+
+    half* x_head = x + (blockIdx.x * gridDim.y * n_heads + blockIdx.y * n_heads + head_idx)*head_dim;
+
+    float freq = 1.0f / pow(freq_base, 2.0f * pair_idx / rotary_dim);
+    float angle = pos * freq;
+
+    float x_0 = __half2float(x_head[pair_idx]);
+    float x_1 = __half2float(x_head[pair_idx+rotary_dim/2]);
+
+    x_head[pair_idx] = __float2half(cos(angle)*x_0 - sin(angle)*x_1);
+    x_head[pair_idx+rotary_dim/2] = __float2half(sin(angle)*x_0 + cos(angle)*x_1);
+}
+
+void neox_rope_cuda(
+    at::Tensor& x, // [B,L,n_heads,head_dim]
+    int rotary_dim,
+    int head_dim,
+    int start_pos,
+    float freq_base
+) {
+    TORCH_CHECK(rotary_dim <= head_dim);
+    TORCH_CHECK(x.dtype() == at::kHalf);
+    TORCH_CHECK(x.dim() == 4);
+    TORCH_CHECK(x.is_contiguous());
+
+    half* x_ptr = x.data_ptr<at::Half>();
+
+    int B = x.size(0);
+    int L = x.size(1);
+    int n_heads = x.size(2);
+    int head_dim = x.size(3);
+
+    dim3 grid(B, L, n_heads*(rotary_dim/2+31)/32);
+
+    cudaStream_t stream = at::cuda::getCurrentCUDAStream();
+    neox_rope_cuda_impl<<<grid, 32, 0, stream>>>(x_ptr, n_heads, rotary_dim, head_dim, start_pos, freq_base);
 }
 
 void matmul_cuda() {
