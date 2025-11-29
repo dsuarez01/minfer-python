bc19d28446b774a740b5bd51adb3bfca18830919
DS <dsuarez@mit.edu>
2025-11-29 01:23:51 -0500

    Added rmsnorm CUDA

--- src/minfer/kernels/torch_ext/csrc/cuda/kernels.cu
+++ src/minfer/kernels/torch_ext/csrc/cuda/kernels.cu
@@ -4,13 +4,79 @@
 
 #include <cuda.h>
 #include <cuda_runtime.h>
-// #include <ATen/cuda/CUDAContext.h>
+#include <ATen/cuda/CUDAContext.h>
 
 // TODO: complete me!
 namespace minfer {
 
-void rmsnorm_cuda() {
-    TORCH_CHECK(false, "rmsnorm not implemented");
+__device__ float blockreduce_sum(float* vs, float v, int tid) {
+    v = warp_reduce_sum(v);
+    if (tid%32 == 0) vs[tid/32] = v;
+    __syncthreads();
+    v = vs[tid%32];
+    return warp_reduce_sum(v);
+}
+
+__device__ float blockreduce_max(float* vs, float v, int tid) {
+    v = warp_reduce_max(v);
+    if (tid%32 == 0) vs[tid/32] = v;
+    __syncthreads();
+    v = vs[tid%32];
+    return warp_reduce_max(v);
+}
+
+__global__ void rmsnorm_cuda_impl(
+    const half* __restrict__ in,
+    half* __restrict__ out,
+    const half* __restrict__ w,
+    int dim,
+    float eps
+) {
+
+    const half* head_in = in + blockIdx.x * dim;
+    half* head_out = out + blockIdx.x * dim;
+    
+    __shared__ float shared_sum[32];
+    
+    // one pass for the squared sum (then parallel reduction over block)
+    float sum_sq = 0.0f;
+    for (int i = threadIdx.x; i<dim; i += blockDim.x) {
+        float val = __half2float(head_in[i]);
+        sum_sq += val*val;
+    }
+
+    sum_sq = blockreduce_sum(shared_sum, sum_sq, threadIdx.x);
+    float sc = rsqrt(sum_sq / float(dim) + eps);
+    
+    // one pass to apply weight and scale
+    for (int i = threadIdx.x; i<dim; i += blockDim.x) {
+        float in_f = __half2float(head_in[i]);
+        float w_f = __half2float(w[i]);
+        head_out[i] = __float2half(in_f*w_f*sc);
+    }
+}
+
+void rmsnorm_cuda(const at::Tensor& in, at::Tensor& out, const at::Tensor& w, int dim, float eps) {
+    // checks
+    TORCH_CHECK(in.size(0) == out.size(0));
+    TORCH_CHECK(in.size(0) == w.size(0) || in.size(0) % w.size(0) == 0)
+    TORCH_CHECK(in.dtype() == at::kHalf);
+    TORCH_CHECK(out.dtype() == at::kHalf);
+    TORCH_CHECK(w.dtype() == at::kHalf);
+    TORCH_INTERNAL_ASSERT(in.device().type() == at::DeviceType::CUDA);
+    TORCH_INTERNAL_ASSERT(out.device().type() == at::DeviceType::CUDA);
+    
+    TORCH_CHECK(in.is_contiguous());
+    TORCH_CHECK(out.is_contiguous());
+
+    const half* in_ptr = in.data_ptr<at::Half>();
+    half* out_ptr = out.data_ptr<at::Half>();
+    const half* w_ptr = out.data_ptr<at::Half>();
+
+    const int n_heads = in.size(0) / w.size(0);
+
+    cudaStream_t stream = at::cuda::getCurrentCUDAStream();
+    rmsnorm_cuda_impl<<<n_heads, 1024, 0, stream>>>(in_ptr, out_ptr, w_ptr, dim, eps);
 }
 
 void il_rope_cuda() {
