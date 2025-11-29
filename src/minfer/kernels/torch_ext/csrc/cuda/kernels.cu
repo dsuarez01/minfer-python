b0f3568770406e026cd56318a2fb5d71e82bba73
DS <dsuarez@mit.edu>
2025-11-29 01:57:56 -0500

    Minor fixes to rmsnorm

--- src/minfer/kernels/torch_ext/csrc/cuda/kernels.cu
+++ src/minfer/kernels/torch_ext/csrc/cuda/kernels.cu
@@ -33,15 +33,15 @@ __global__ void rmsnorm_cuda_impl(
     float eps
 ) {
 
-    const half* head_in = in + blockIdx.x * dim;
-    half* head_out = out + blockIdx.x * dim;
+    const half* vec_in = in + blockIdx.x * dim;
+    half* vec_out = out + blockIdx.x * dim;
     
     __shared__ float shared_sum[32];
     
     // one pass for the squared sum (then parallel reduction over block)
     float sum_sq = 0.0f;
     for (int i = threadIdx.x; i<dim; i += blockDim.x) {
-        float val = __half2float(head_in[i]);
+        float val = __half2float(vec_in[i]);
         sum_sq += val*val;
     }
 
@@ -50,16 +50,16 @@ __global__ void rmsnorm_cuda_impl(
     
     // one pass to apply weight and scale
     for (int i = threadIdx.x; i<dim; i += blockDim.x) {
-        float in_f = __half2float(head_in[i]);
+        float in_f = __half2float(vec_in[i]);
         float w_f = __half2float(w[i]);
-        head_out[i] = __float2half(in_f*w_f*sc);
+        vec_out[i] = __float2half(in_f*w_f*sc);
     }
 }
 
-void rmsnorm_cuda(const at::Tensor& in, at::Tensor& out, const at::Tensor& w, int dim, float eps) {
+void rmsnorm_cuda(const at::Tensor& in, at::Tensor& out, const at::Tensor& w, float eps) {
     // checks
-    TORCH_CHECK(in.size(0) == out.size(0));
-    TORCH_CHECK(in.size(0) == w.size(0) || in.size(0) % w.size(0) == 0)
+    TORCH_CHECK(in.shape() == out.shape());
+    TORCH_CHECK(in.size(-1) == w.size(0) || in.size(-1) % w.size(0) == 0);
     TORCH_CHECK(in.dtype() == at::kHalf);
     TORCH_CHECK(out.dtype() == at::kHalf);
     TORCH_CHECK(w.dtype() == at::kHalf);
@@ -68,18 +68,26 @@ void rmsnorm_cuda(const at::Tensor& in, at::Tensor& out, const at::Tensor& w, in
     
     TORCH_CHECK(in.is_contiguous());
     TORCH_CHECK(out.is_contiguous());
+    TORCH_CHECK(w.is_contiguous());
 
+    int dim = w.size(0);
     const half* in_ptr = in.data_ptr<at::Half>();
     half* out_ptr = out.data_ptr<at::Half>();
-    const half* w_ptr = out.data_ptr<at::Half>();
+    const half* w_ptr = w.data_ptr<at::Half>();
 
-    const int n_heads = in.size(0) / w.size(0);
+    const int n_norms = in.size(0) * in.size(1) * (in.size(-1) / dim);
 
     cudaStream_t stream = at::cuda::getCurrentCUDAStream();
-    rmsnorm_cuda_impl<<<n_heads, 1024, 0, stream>>>(in_ptr, out_ptr, w_ptr, dim, eps);
+    rmsnorm_cuda_impl<<<n_norms, 1024, 0, stream>>>(in_ptr, out_ptr, w_ptr, dim, eps);
 }
 
-void il_rope_cuda() {
+void il_rope_cuda(
+    const at::Tensor& x,
+    int d_rotary,
+    int d_head,
+    float freq_base,
+
+) {
     TORCH_CHECK(false, "il_rope not implemented");
 }
 
