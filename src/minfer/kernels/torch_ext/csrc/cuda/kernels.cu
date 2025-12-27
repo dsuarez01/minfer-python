66f2e5c96e063bfbba7bc5543f9adbd2f95c6f58
DS <dsuarez@mit.edu>
2025-12-27 03:22:25 -0500

    Rework of flash attn mostly done

--- src/minfer/kernels/torch_ext/csrc/cuda/kernels.cu
+++ src/minfer/kernels/torch_ext/csrc/cuda/kernels.cu
@@ -1132,87 +1132,42 @@ void qkv_cuda(
     }
 }
 
-// helper for moe scoring and flash attn, where (online) softmax is fused and taken per-tile
-// first thread in warps responsible for writing warp vals (d and m) [per-row]
-// threads in first warp responsible for reducing over warp vals [per-row]
-template <int THR_PER_ROW>
-static __device__ float row_reduce_max(float* vs, float v, int row_in_tile, int thr_in_row) {
-
-    v = warp_reduce_max<THR_PER_ROW>(v);
-
-    constexpr int WARPS_PER_ROW = (THR_PER_ROW+32-1)/32;
-
-    int warp_in_row = thr_in_row / 32;
-    if (thr_in_row % 32 == 0) vs[row_in_tile*WARPS_PER_ROW+warp_in_row]=v;
-    __syncthreads();
-    
-    if (THR_PER_ROW/32 > 0 && warp_in_row == 0 && thr_in_row < WARPS_PER_ROW) {
-        v = vs[row_in_tile*WARPS_PER_ROW + thr_in_row];
-        v = warp_reduce_max(v);
-    }
-
-    if (thr_in_row == 0) vs[row_in_tile*WARPS_PER_ROW] = v;
-    __syncthreads();
-    return vs[row_in_tile*WARPS_PER_ROW];
-}
-
-template <int THR_PER_ROW>
-static __device__ float row_reduce_sum(float* vs, float v, int row_in_tile, int thr_in_row) {
-
-    v = warp_reduce_sum<THR_PER_ROW>(v);
-
-    constexpr int WARPS_PER_ROW = (THR_PER_ROW+32-1)/32;
-
-    int warp_in_row = thr_in_row / 32;
-    if (thr_in_row % 32 == 0) vs[row_in_tile*WARPS_PER_ROW+warp_in_row]=v;
-    __syncthreads();
-    
-    if (THR_PER_ROW/32 > 0 && warp_in_row == 0 && thr_in_row < WARPS_PER_ROW) {
-        v = vs[row_in_tile*WARPS_PER_ROW + thr_in_row];
-        v = warp_reduce_sum(v);
-    }
-
-    if (thr_in_row == 0) vs[row_in_tile*WARPS_PER_ROW] = v;
-    __syncthreads();
-    return vs[row_in_tile*WARPS_PER_ROW];
-}
-
-template <int TILE_M, int TILE_N, int THR_PER_ROW>
+// helper (TODO: complete me once you know exactly where this will be used)
+template <int WARPS_PER_BLOCK>
 static __device__ void update_dm(
     float* __restrict__ ds,
     float* __restrict__ ms,
-    float* __restrict__ scratch_d,
-    float* __restrict__ scratch_m,
-    const half* __restrict__ tile
+    const half (&tile)[WARPS_PER_BLOCK*WMMA_M][WMMA_N]
 ) {
+    int warpId = threadIdx.x/32;
+    int lane = threadIdx.x%32;
+    int rowInWarp = lane/2;
+    int thrInRow = lane%2;
+    int globalRow = warpId*WMMA_M+rowInWarp;
 
-    int row_in_tile = threadIdx.x/THR_PER_ROW;
-    int thr_in_row = threadIdx.x%THR_PER_ROW;
-
-    // compute tile's m vals per row, write to tiles_m
-    float ml = -INFINITY;
-    for (int col=thr_in_row; col<TILE_N; col+=THR_PER_ROW) {
-        ml = fmaxf(ml, __half2float(tile[row_in_tile*TILE_N+col])); 
+    // row max
+    float localMax = -INFINITY;
+    for (int c=thrInRow; c<WMMA_N; c+=2) {
+        localMax = fmaxf(localMax, __half2float(tile[globalRow][c]));
     }
+    float partnerMax = __shfl_xor_sync(0xffffffff, localMax, 1);
+    float row_m = fmaxf(localMax, partnerMax);
 
-    float row_m = row_reduce_max<THR_PER_ROW>(scratch_m, ml, row_in_tile, thr_in_row);
-
-    // compute tile's d vals per row, write to tiles_d
-    float dl = 0.0f;
-    for (int col=thr_in_row; col<TILE_N; col+=THR_PER_ROW) {
-        dl += expf(__half2float(tile[row_in_tile*TILE_N+col])-row_m);
+    // row sum
+    float localSum = 0.0f;
+    for (int c=thrInRow; c<WMMA_N; c+=2) {
+        localSum += expf(__half2float(tile[globalRow][c])-row_m);
     }
+    float partnerSum = __shfl_xor_sync(0xffffffff, localSum, 1);
+    float row_d = localSum + partnerSum;
 
-    float row_d = row_reduce_sum<THR_PER_ROW>(scratch_d, dl, row_in_tile, thr_in_row);
-
-    // update m and d
-    if (thr_in_row == 0) {
-        float row_m_old = ms[row_in_tile];
-        float row_d_old = ds[row_in_tile];
-        float row_m_new = fmaxf(row_m_old, row_m);
-        float row_d_new = row_d_old*expf(row_m_old-row_m_new)+row_d*expf(row_m-row_m_new);
-        ms[row_in_tile] = row_m_new;
-        ds[row_in_tile] = row_d_new;
+    if (thrInRow == 0) {
+        float m_old = ms[globalRow];
+        float d_old = ds[globalRow];
+        float m_new = fmaxf(m_old, row_m);
+        float d_new = d_old*expf(m_old-m_new) + row_d*expf(row_m-m_new);
+        ms[globalRow] = m_new;
+        ds[globalRow] = d_new;
     }
 }
 
@@ -1234,9 +1189,6 @@ __global__ void flash_attn_cuda_impl(
     half* __restrict__ out
 ) {
     
-    constexpr int THR_PER_ROW = BLOCK_SIZE / TILE_M;
-    constexpr int WARPS_PER_ROW = (THR_PER_ROW+32-1)/32;
-
     __shared__ half l_shared[WARPS_PER_BLOCK*WMMA_M][WMMA_N];
     __shared__ half pv_shared[WARPS_PER_BLOCK*WMMA_M][HEAD_DIM];
     __shared__ half out_shared[WARPS_PER_BLOCK*WMMA_M][HEAD_DIM];
@@ -1253,68 +1205,61 @@ __global__ void flash_attn_cuda_impl(
     int64_t kv_head_idx = q_head_idx / (n_heads/n_kv_heads);
     int64_t seq_tile = blockIdx.z;
 
-    const half* q_row = q + batch_idx*strides.q0 + q_head_idx*strides.q1 + seq_tile*TILE_M*strides.q2;
+    const half* q_row = q + batch_idx*strides.q0 + q_head_idx*strides.q1 + seq_tile*WARPS_PER_BLOCK*WMMA_M*strides.q2;
     const half* k_head = k + batch_idx*strides.kv0 + kv_head_idx*strides.kv1;
     const half* v_head = v + batch_idx*strides.kv0 + kv_head_idx*strides.kv1;
     
-    half* out_row = out + batch_idx*n_heads*L*HEAD_DIM + q_head_idx*L*HEAD_DIM + seq_tile*TILE_M*HEAD_DIM;
-    const uint8_t* mask_row = mask + batch_idx*L*L + seq_tile*TILE_M*L; // [B, 1, L, L]
+    half* out_row = out + batch_idx*n_heads*L*HEAD_DIM + q_head_idx*L*HEAD_DIM + seq_tile*WARPS_PER_BLOCK*WMMA_M*HEAD_DIM;
+    const uint8_t* mask_row = mask + batch_idx*L*L + seq_tile*WARPS_PER_BLOCK*WMMA_M*L; // [B, 1, L, L]
 
-    for (int idx=threadIdx.x; idx<TILE_M; idx+=blockDim.x) {
+    for (int idx=threadIdx.x; idx<WARPS_PER_BLOCK*WMMA_M; idx+=blockDim.x) {
         ms[idx] = HALF_MIN;
         ds[idx] = 0.0f;
     }
 
-    for (int idx=threadIdx.x; idx<TILE_M*HEAD_DIM; idx+=blockDim.x) {
+    for (int idx=threadIdx.x; idx<WARPS_PER_BLOCK*WMMA_M*HEAD_DIM; idx+=blockDim.x) {
         out_shared[idx/HEAD_DIM][idx%HEAD_DIM] = __float2half(0.0f);
     }
 
     __syncthreads();
 
-    for (int n_tile=0; n_tile<L; n_tile+=TILE_N) {
+    for (int n_tile=0; n_tile<L; n_tile+=WMMA_N) {
         const half* k_row = k_head + n_tile*strides.kv2;
         const half* v_row = v_head + n_tile*strides.kv2;
         const uint8_t* mask_tile = mask_row + n_tile;
 
-        compute_tile_wmma_f16<TILE_M, TILE_N, HEAD_DIM>(
-            HEAD_DIM, 
-            strides.q2,
-            TILE_N,
-            strides.kv2,
-            (half*)in_shared, (float*)qk_acc_shared, (half*)kv_shared,
-            q_row, (half*)l_shared, k_row
+        compute_tile_wmma_f16(
+            L, HEAD_DIM, L,
+            strides.q2, strides.kv2, WMMA_N,
+            q_row, k_row, (half*)l_shared
         );
 
         // apply mask and scale before computing ms and ds
-        for (int idx=threadIdx.x; idx<TILE_M*TILE_N; idx+=blockDim.x) {
-            int r = idx / TILE_N;
-            int c = idx % TILE_N;
+        for (int idx=threadIdx.x; idx<WARPS_PER_BLOCK*WMMA_M*WMMA_N; idx+=blockDim.x) {
+            int r = idx / WMMA_N;
+            int c = idx % WMMA_N;
 
             l_shared[r][c] = mask_tile[r*L+c] ? __float2half(__half2float(l_shared[r][c]) / sqrtf((float)HEAD_DIM)) : __float2half(HALF_MIN);
         }
 
-        __syncthreads();
-
-        for (int idx=threadIdx.x; idx<TILE_M; idx+=blockDim.x) {
+        for (int idx=threadIdx.x; idx<WARPS_PER_BLOCK*WMMA_M; idx+=blockDim.x) {
             ms_old[idx] = ms[idx];
             ds_old[idx] = ds[idx];
         }
 
         __syncthreads();
 
-        update_dm<TILE_M, TILE_N, THR_PER_ROW>(ds, ms, scratch_d, scratch_m, (half*)l_shared);
+        update_dm<WARPS_PER_BLOCK>(ds, ms, l_shared);
 
         __syncthreads();
 
-        for (int idx=threadIdx.x; idx<TILE_M*TILE_N; idx+=blockDim.x) {
-            int r = idx/TILE_N;
-            int c = idx%TILE_N;
+        for (int idx=threadIdx.x; idx<WARPS_PER_BLOCK*WMMA_M*WMMA_N; idx+=blockDim.x) {
+            int r = idx/WMMA_N;
+            int c = idx%WMMA_N;
             l_shared[r][c] = __float2half(expf(__half2float(l_shared[r][c])-ms[r]));
         }
 
-        __syncthreads();
-
-        for (int idx=threadIdx.x; idx<TILE_M*HEAD_DIM; idx+=blockDim.x) {
+        for (int idx=threadIdx.x; idx<WARPS_PER_BLOCK*WMMA_M*HEAD_DIM; idx+=blockDim.x) {
             int r = idx/HEAD_DIM;
             int c = idx%HEAD_DIM;
 
@@ -1323,28 +1268,26 @@ __global__ void flash_attn_cuda_impl(
 
         __syncthreads();
 
-        // weird but essentially utilizing V^T^T = V since we are computing l_shared @ V, not V^T
-        // can optimize this later by writing a separate impl that handles not writing shared mem to itself (l_shared), 
-        // just wanted to reuse some code for now and deal w it later during profiling
-        compute_tile_wmma_f16<TILE_M, HEAD_DIM, TILE_N, true, true>(
-            TILE_N,
-            TILE_N,
-            HEAD_DIM,
-            strides.kv2,
-            (half*)l_shared, (float*)out_acc_shared, (half*)kv_shared,
-            (half*)l_shared, (half*)pv_shared, v_row
-        );
+
+        #pragma unroll
+        for (int n=0; n<HEAD_DIM; n+=WMMA_N) {
+            compute_tile_wmma_f16_T(
+                WARPS_PER_BLOCK*WMMA_M, WMMA_N, HEAD_DIM,
+                WMMA_N, strides.kv2, HEAD_DIM,
+                (half*)l_shared, v_row + n,  &pv_shared[0][n]
+            );
+        }
 
         __syncthreads();
 
-        for (int idx=threadIdx.x; idx<TILE_M*HEAD_DIM; idx+=blockDim.x) {
+        for (int idx=threadIdx.x; idx<WARPS_PER_BLOCK*WMMA_M*HEAD_DIM; idx+=blockDim.x) {
             int r = idx/HEAD_DIM;
             int c = idx%HEAD_DIM;
             out_shared[r][c] = __float2half(__half2float(out_shared[r][c]) + __half2float(pv_shared[r][c]));
         }
     }
 
-    for (int idx=threadIdx.x; idx<TILE_M*HEAD_DIM; idx+=blockDim.x) {
+    for (int idx=threadIdx.x; idx<WARPS_PER_BLOCK*WMMA_M*HEAD_DIM; idx+=blockDim.x) {
         int r = idx/HEAD_DIM;
         int c = idx%HEAD_DIM;
         out_row[r*HEAD_DIM+c] = (ds[r] > 0.0f) ? __float2half(__half2float(out_shared[r][c]) / ds[r]) : __float2half(0.0f);
@@ -1434,7 +1377,7 @@ void flash_attn_cuda(
     constexpr int BLOCK_SIZE = WARPS_PER_BLOCK*32;
     constexpr int ROWS_M = WARPS_PER_BLOCK*WMMA_M;
     constexpr int HEAD_DIM = 128;
-    dim3 grid(B, n_heads, (M+ROWS_M-1)/ROWS_M);
+    dim3 grid(B, n_heads, (L+ROWS_M-1)/ROWS_M);
     dim3 block(BLOCK_SIZE);
 
     flash_attn_cuda_impl<WARPS_PER_BLOCK, HEAD_DIM><<<grid, block, 0, stream>>>(
